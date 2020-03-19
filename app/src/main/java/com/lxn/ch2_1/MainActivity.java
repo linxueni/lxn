aaf76b49a7502ead104687ef01b033e867ad82c0
linxueni <2849773560@qq.com>
2020-03-19 10:53:39 +0800

    当前类监听

--- app/src/main/java/com/lxn/ch2_1/MainActivity.java
+++ app/src/main/java/com/lxn/ch2_1/MainActivity.java
@@ -1,15 +1,29 @@
 package com.lxn.ch2_1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.util.Log;
+import android.widget.TextView;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+     TextView out;
+     EditText edit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //add something..
+        out= findViewById(R.id.textView);
+        edit= findViewById(R.id.editText);
+        Button btn= findViewById(R.id.button);
+        btn.setOnClickListener(this);
+    }
+    public void onClick(View v){
+        Log.i("main","onClick msg....");
+        String str=edit.getText().toString();
+        out.setText(str);
     }
 }
