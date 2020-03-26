e6712f86c3037f7656c84702f48f161e6bea6ff9
linxueni <2849773560@qq.com>
2020-03-26 14:10:56 +0800

    两队记分器练习

--- app/src/main/java/com/lxn/ch2_1/SecondActivity.java
+++ app/src/main/java/com/lxn/ch2_1/SecondActivity.java
@@ -9,28 +9,49 @@ import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
  TextView score;
+ TextView score2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-        score=findViewById(R.id.score);
+        score=findViewById(R.id.score1);
+        score2=findViewById(R.id.score2);
     }
     public void btnAdd1(View v) {
+        if(v.getId()==R.id.button_4)
       showScore(1);
+        else{
+            showScore2(1);
+        }
     }
     public void btnAdd2(View v) {
-      showScore(2);
+        if(v.getId()==R.id.button_5)
+            showScore(2);
+        else{
+            showScore2(2);
+        }
     }
     public void btnAdd3(View v) {
-      showScore(3);
+        if(v.getId()==R.id.button_6)
+            showScore(3);
+        else{
+            showScore2(3);
+        }
     }
     public void reset(View v) {
         score.setText("0");
+        score2.setText("0");
     }
     public void showScore(int inc) {
         Log.i("main","inc="+inc);
      String oldScore=(String) score.getText();
      int newScore=Integer.parseInt(oldScore)+inc;
      score.setText(""+newScore);
+    }
+    public void showScore2(int inc) {
+        Log.i("main","inc="+inc);
+        String oldScore=(String) score2.getText();
+        int newScore=Integer.parseInt(oldScore)+inc;
+        score2.setText(""+newScore);
     }
 }
